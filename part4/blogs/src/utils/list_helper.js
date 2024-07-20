c408ce177ed0d09e9e321e89623753090f4a30cf
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-20 23:35:16 +0500

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/blogs/src/utils/list_helper.js
+++ part4/blogs/src/utils/list_helper.js
@@ -6,6 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, blog) => acc + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  return blogs.reduce((acc, blog) => acc.likes > blog.likes ? acc : blog, blogs[0]);
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy,
+  totalLikes,
+  favoriteBlog
 }

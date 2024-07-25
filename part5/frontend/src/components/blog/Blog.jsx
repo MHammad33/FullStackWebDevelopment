f14b15c82adb9ae2319e40021e7070d25abc0105
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-25 06:22:15 +0500

    5.7 Blog List Frontend, step 7

--- part5/frontend/src/components/blog/Blog.jsx
+++ part5/frontend/src/components/blog/Blog.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import "./Blog.css";
 
-const Blog = ({ blog }) => (
-	<div className="blog-card">
-		<div className="blog-title-container">
-			<h3 className="blog-title">{blog.title}</h3>
+const Blog = ({ blog }) => {
+	const [fullBlogVisible, setFullBlogVisible] = useState(false);
+
+	const toggleVisibility = () => {
+		setFullBlogVisible(!fullBlogVisible);
+	};
+
+	return (
+		<div className="blog-card">
+			<div className="blog-header">
+				<div className="blog-title-container">
+					<h3 className="blog-title">{blog.title}</h3>
+					<p className="blog-author">by {blog.author}</p>
+				</div>
+				<button
+					className={`${
+						fullBlogVisible ? "blog-hide-button" : "blog-view-button"
+					}`}
+					onClick={toggleVisibility}
+				>
+					{fullBlogVisible ? "Hide" : "View"}
+				</button>
+			</div>
+			{fullBlogVisible && (
+				<div className="blog-full">
+					<p>
+						<a href={blog.url}>Click here to see full blog</a>
+					</p>
+					<p>
+						{blog.likes} likes <button className="like-button">Like</button>
+					</p>
+					<p>Added by {blog.user.username}</p>
+				</div>
+			)}
 		</div>
-		<p className="blog-author">by {blog.author}</p>
-	</div>
-);
+	);
+};
 
 export default Blog;

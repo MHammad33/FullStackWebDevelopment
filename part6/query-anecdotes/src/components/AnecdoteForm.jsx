311de1f92772cfb4b9f61915aac8e0a3fcabb692
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-13 07:50:56 +0500

    Exercise 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,46 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
 const AnecdoteForm = () => {
+	const queryClient = useQueryClient();
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+	const newAnecdoteMutation = useMutation({
+		mutationFn: (newAnecdote) =>
+			axios
+				.post("http://localhost:3001/anecdotes", newAnecdote)
+				.then((res) => res.data),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+		},
+		onError: (error) => {
+			console.error("Error creating anecdote:", error.message);
+		},
+	});
 
-  return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
-}
+	const onCreate = (event) => {
+		event.preventDefault();
+		const content = event.target.anecdote.value;
+		event.target.anecdote.value = "";
+		console.log("new anecdote");
+		const newAnecdote = {
+			content,
+			id: Date.now(),
+			votes: 0,
+		};
 
-export default AnecdoteForm
+		console.log("newAnecdote", newAnecdote);
+		newAnecdoteMutation.mutate(newAnecdote);
+	};
+
+	return (
+		<div>
+			<h3>create new</h3>
+			<form onSubmit={onCreate}>
+				<input name="anecdote" />
+				<button type="submit">create</button>
+			</form>
+		</div>
+	);
+};
+
+export default AnecdoteForm;

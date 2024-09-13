30037e26fab0c9f29e6f2285164bd9d637501d8e
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-13 07:34:43 +0500

    Exercise 6.20
    Implement retrieving anecdotes from the server using React Query.

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,40 +1,52 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
 
 const App = () => {
+	const result = useQuery({
+		queryKey: ["anecdotes"],
+		queryFn: () =>
+			axios.get("http://localhost:3001/anecdotes").then((res) => res.data),
+		retry: 1,
+	});
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+	// const handleVote = (anecdote) => {
+	// 	console.log("vote");
+	// };
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+	// const anecdotes = [
+	// 	{
+	// 		content: "If it hurts, do it more often",
+	// 		id: "47145",
+	// 		votes: 0,
+	// 	},
+	// ];
 
-  return (
-    <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
-    </div>
-  )
-}
+	if (result.isLoading) {
+		return <h3>Loading...</h3>;
+	}
 
-export default App
+	const anecdotes = result.data;
+
+	return (
+		<div>
+			<h3>Anecdote app</h3>
+
+			<Notification />
+			<AnecdoteForm />
+
+			{anecdotes.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => handleVote(anecdote)}>vote</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default App;

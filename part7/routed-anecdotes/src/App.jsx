da811ae024121cf657907b1c1bd78a2e7e7c6e8b
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-16 15:14:56 +0500

    7.2: Routed Anecdotes, step 2

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useMatch } from "react-router-dom";
 
 const Menu = () => {
 	const padding = {
@@ -25,7 +25,9 @@ const AnecdoteList = ({ anecdotes }) => (
 		<h2>Anecdotes</h2>
 		<ul>
 			{anecdotes.map((anecdote) => (
-				<li key={anecdote.id}>{anecdote.content}</li>
+				<li key={anecdote.id}>
+					<Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+				</li>
 			))}
 		</ul>
 	</div>
@@ -113,6 +115,18 @@ const CreateNew = (props) => {
 	);
 };
 
+const Anecdote = ({ anecdote }) => (
+	<div>
+		<h2>
+			{anecdote.content} by {anecdote.author}
+		</h2>
+		<p>had {anecdote.votes} votes</p>
+		<p>
+			for more info see <a href={anecdote.info}>{anecdote.info}</a>
+		</p>
+	</div>
+);
+
 const App = () => {
 	const [anecdotes, setAnecdotes] = useState([
 		{
@@ -151,6 +165,11 @@ const App = () => {
 		setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
 	};
 
+	const match = useMatch("/anecdotes/:id");
+	const anecdote = match
+		? anecdotes.find((anecdote) => anecdote.id === Number(match.params.id))
+		: null;
+
 	return (
 		<div>
 			<h1>Software anecdotes</h1>
@@ -159,6 +178,10 @@ const App = () => {
 
 			<Routes>
 				<Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+				<Route
+					path="/anecdotes/:id"
+					element={<Anecdote anecdote={anecdote} />}
+				/>
 				<Route path="/about" element={<About />} />
 				<Route path="/create" element={<CreateNew addNew={addNew} />} />
 			</Routes>

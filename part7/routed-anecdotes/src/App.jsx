df20ec9a5fafbc3a3f3ac3f27b8c6797507d7f2f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-16 15:19:32 +0500

    7.3: Routed Anecdotes, step3

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route, Link, useMatch } from "react-router-dom";
+import { Routes, Route, Link, useMatch, useNavigate } from "react-router-dom";
 
 const Menu = () => {
 	const padding = {
@@ -26,7 +26,7 @@ const AnecdoteList = ({ anecdotes }) => (
 		<ul>
 			{anecdotes.map((anecdote) => (
 				<li key={anecdote.id}>
-					<Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+					<Link to={`/anecdotes/  ${anecdote.id}`}>{anecdote.content}</Link>
 				</li>
 			))}
 		</ul>
@@ -70,6 +70,7 @@ const CreateNew = (props) => {
 	const [content, setContent] = useState("");
 	const [author, setAuthor] = useState("");
 	const [info, setInfo] = useState("");
+	const navigate = useNavigate();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -79,6 +80,8 @@ const CreateNew = (props) => {
 			info,
 			votes: 0,
 		});
+
+		navigate("/");
 	};
 
 	return (
@@ -150,6 +153,11 @@ const App = () => {
 	const addNew = (anecdote) => {
 		anecdote.id = Math.round(Math.random() * 10000);
 		setAnecdotes(anecdotes.concat(anecdote));
+		setNotification(`a new anecdote ${anecdote.content} created`);
+
+		setTimeout(() => {
+			setNotification("");
+		}, 5000);
 	};
 
 	const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
@@ -175,6 +183,9 @@ const App = () => {
 			<h1>Software anecdotes</h1>
 
 			<Menu />
+			{notification && (
+				<p style={{ border: "2px solid red" }}>{notification}</p>
+			)}
 
 			<Routes>
 				<Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />

421e2dce41896371049baff8ced9f8bf61918cb4
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-22 14:04:46 +0500

    Clean App.jsx

--- part7/blogs-app/frontend/src/App.jsx
+++ part7/blogs-app/frontend/src/App.jsx
@@ -1,18 +1,13 @@
-import { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Login from "./components/login/Login";
 import Notification from "./components/Notification";
-import useAuth from "./hooks/useAuth";
 import UserInfo from "./components/UserInfo";
 import UsersList from "./components/users/UsersList";
 import UserDetail from "./components/users/UserDetail";
 import BlogList from "./components/BlogList";
 
 const App = () => {
-  const navigate = useNavigate();
-
   return (
     <div>
       <Notification />

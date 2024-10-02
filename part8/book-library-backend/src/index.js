81ce3cc7c155c5545ed92ac0a27314d33d632b3c
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-02 18:50:03 +0500

    Setup GraphQl server
    Setup GraphQl server using Apollo Server

--- part8/book-library-backend/src/index.js
+++ part8/book-library-backend/src/index.js
@@ -1,13 +1,23 @@
-const app = require("./app");
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 const config = require("./utils/config");
 const connectDb = require("./db/connectDb");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 
 const startServer = async () => {
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+	});
+
 	try {
 		await connectDb(config.MONGODB_URI);
-		app.listen(config.PORT, () =>
-			console.log("Listening on port:", config.PORT)
-		);
+		startStandaloneServer(server, {
+			listen: { port: 4000 },
+		}).then(({ url }) => {
+			console.log(`Server ready at ${url}`);
+		});
 	} catch (error) {
 		console.error("Error starting server...", error);
 	}

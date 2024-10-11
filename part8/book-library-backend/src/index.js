ee5fcd236612749ea57b1425a5724d4bbae12bb2
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-11 19:00:30 +0500

    Add websocket server

--- part8/book-library-backend/src/index.js
+++ part8/book-library-backend/src/index.js
@@ -4,6 +4,9 @@ const { makeExecutableSchema } = require("@graphql-tools/schema");
 const {
 	ApolloServerPluginDrainHttpServer,
 } = require("@apollo/server/plugin/drainHttpServer");
+const { WebSocketServer } = require("ws");
+const { useServer } = require("graphql-ws/lib/use/ws");
+
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
@@ -22,9 +25,28 @@ const startServer = async () => {
 	const app = express();
 	const httpServer = http.createServer(app);
 
+	const wsServer = new WebSocketServer({
+		server: httpServer,
+		path: "/",
+	});
+
+	const schema = makeExecutableSchema({ typeDefs, resolvers });
+	const serverCleanup = useServer({ schema }, wsServer);
+
 	const server = new ApolloServer({
-		schema: makeExecutableSchema({ typeDefs, resolvers }),
-		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+		schema,
+		plugins: [
+			ApolloServerPluginDrainHttpServer({ httpServer }),
+			{
+				async serverWillStart() {
+					return {
+						async drainServer() {
+							await serverCleanup.dispose();
+						},
+					};
+				},
+			},
+		],
 	});
 
 	await server.start();

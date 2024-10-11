33e44c8bbc9c4680bda3554f26af4e8a9e9afba6
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-11 18:43:20 +0500

    Remove previous setup of graphql

--- part8/book-library-backend/src/index.js
+++ part8/book-library-backend/src/index.js
@@ -1,5 +1,4 @@
 const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
 const { expressMiddleware } = require("@apollo/server/express4");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const {
@@ -16,30 +15,16 @@ const connectDb = require("./db/connectDb");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
-const startServer = async () => {
-	const server = new ApolloServer({
-		typeDefs,
-		resolvers,
+connectDb(config.MONGODB_URI)
+	.then((response) => {
+		console.log("Connection to Database");
+	})
+	.catch((error) => {
+		console.log("Error connecting to database", error);
 	});
 
-	try {
-		await connectDb(config.MONGODB_URI);
-		startStandaloneServer(server, {
-			listen: { port: 4000 },
-			context: async ({ req, res }) => {
-				const auth = req ? req.headers.authorization : null;
-				if (auth && auth.startsWith("Bearer ")) {
-					const decodedToken = jwt.verify(auth.substring(7), "secret");
-					const currentUser = await User.findById(decodedToken.id);
-					return { currentUser };
-				}
-			},
-		}).then(({ url }) => {
-			console.log(`Server ready at ${url}`);
-		});
-	} catch (error) {
-		console.error("Error starting server...", error);
-	}
+const startServer = async () => {
+	const app = express();
 };
 
 startServer();

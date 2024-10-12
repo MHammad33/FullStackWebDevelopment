4ba9196e5c60ebf9aa54f7789d197d7db072e5bf
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-12 13:07:29 +0500

    Add basic bookAdd subscription setup

--- part8/book-library-frontend/src/App.jsx
+++ part8/book-library-frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect, useMemo } from "react";
-import { useQuery, useApolloClient } from "@apollo/client";
+import { useQuery, useApolloClient, useSubscription } from "@apollo/client";
 
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import Login from "./components/Login";
 import BooksByFavoriteGenre from "./components/BooksByFavoriteGenre";
+import { BOOK_ADDED } from "./queries";
 
 const App = () => {
 	const [page, setPage] = useState("authors");
@@ -13,6 +14,12 @@ const App = () => {
 		() => !!localStorage.getItem("booksLibrary-user-token"),
 		[]
 	);
+
+	useSubscription(BOOK_ADDED, {
+		onData: ({ data }) => {
+			console.log(data);
+		},
+	});
 
 	const client = useApolloClient();
 

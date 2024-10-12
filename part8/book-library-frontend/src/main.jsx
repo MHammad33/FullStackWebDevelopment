d0a49c34b2d8b9c3b2210f281e7950a6b7a3e639
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-12 13:02:30 +0500

    Setup WebSocketServer in frontend

--- part8/book-library-frontend/src/main.jsx
+++ part8/book-library-frontend/src/main.jsx
@@ -30,9 +30,27 @@ const httpLink = createHttpLink({
 	uri: "http://localhost:4000",
 });
 
+const wsLink = new GraphQLWsLink(
+	createClient({
+		url: "http://localhost:4000",
+	})
+);
+
+const splitLink = split(
+	({ query }) => {
+		const definition = getMainDefinition(query);
+		return (
+			definition.kind === "OperationDefinition" &&
+			definition.operation === "subscription"
+		);
+	},
+	wsLink,
+	authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
-	link: authLink.concat(httpLink),
+	link: splitLink,
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(

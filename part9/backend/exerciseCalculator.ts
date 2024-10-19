0e745e9be54f25ca4df85814484e6d42a5e5e6e2
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-19 13:12:10 +0500

    Create Initial Exercise Calculator Function

---
+++ part9/backend/exerciseCalculator.ts
@@ -0,0 +1,11 @@
+interface ExerciseResult {
+	periodLength: number;
+	trainingDays: number;
+	success: boolean;
+	rating: number;
+	ratingDescription: string;
+	target: number;
+	average: number;
+}
+
+const calculateExercises = (exerciseHours: number[]): ExerciseResult => {};

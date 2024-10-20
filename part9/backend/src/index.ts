698b75bec3cfdc6ca949ff0b55e77584b347324f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-20 14:25:47 +0500

    Add /bmi route handler

--- part9/backend/src/index.ts
+++ part9/backend/src/index.ts
@@ -5,6 +5,20 @@ app.get("/hello", (_req, res) => {
 	res.send("Hello Full Stack");
 });
 
+app.get("/bmi", (req, res) => {
+	const { weight, height } = req.query;
+
+	try {
+		if (!weight || !height) {
+			throw new Error("Weight or height cannot be empty");
+		}
+
+		res.json({ weight, height });
+	} catch (error) {
+		res.status(401).json({ msg: error.message });
+	}
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {

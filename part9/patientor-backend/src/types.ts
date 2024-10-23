b95029d25904980f25b26af5e0c7b883bff3e7ae
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-23 18:33:55 +0500

    Add Patient Type

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -3,3 +3,14 @@ export interface DiagnosisInfo {
 	name: string;
 	latin?: string;
 }
+
+export type Gender = "male" | "female" | "other";
+
+export interface PatientRecord {
+	id: string;
+	name: string;
+	dateOfBirth: string;
+	ssn: string;
+	gender: Gender;
+	occupation: string;
+}

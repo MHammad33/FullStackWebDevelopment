49dbc35283602123f4530781afed0cdaa7140a85
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-23 18:42:14 +0500

    Add logic to fetching patients

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import patientService from "../services/patientService";
 
 const patientRouter = Router();
 
 patientRouter.get("/", (_req, res) => {
-	res.send("Fetching all patients");
+	try {
+		const patients = patientService.getAllPatientDetails();
+		res.json(patients);
+	} catch (error) {
+		console.error(error);
+		res
+			.status(500)
+			.json({ message: "An error occurred while fetching diagnoses." });
+	}
 });
 
 export default patientRouter;

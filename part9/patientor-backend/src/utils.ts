45b88004921e20468bb581637928fe9f21caa974
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-25 15:43:41 +0500

    Add validation using type guards

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,1 +1,66 @@
-export const toNewPatientData = (object: any) => {};
+import { Gender, NewPatientInfo } from "./types";
+
+export const toNewPatientData = (object: unknown): NewPatientInfo => {
+	console.log(object);
+	const newPatient: NewPatientInfo = {
+		name: parseName(object.name),
+		dateOfBirth: parseDate(object.dateOfBirth),
+		ssn: parseSSN(object.ssn),
+		gender: parseGender(object.gender),
+		occupation: parseOccupation(object.occupation),
+	};
+
+	return newPatient;
+};
+
+const parseName = (name: unknown) => {
+	if (!name || !isString(name)) {
+		throw new Error("Incorrect or missing name");
+	}
+
+	return name;
+};
+
+const parseOccupation = (occupation: unknown) => {
+	if (!occupation || !isString(occupation)) {
+		throw new Error("Incorrect or missing occupation");
+	}
+
+	return occupation;
+};
+
+const parseSSN = (ssn: unknown) => {
+	if (!ssn || isString(ssn)) {
+		throw new Error("Incorrect or missing ssn");
+	}
+
+	return ssn;
+};
+
+const isString = (text: unknown) => {
+	return typeof text === "string" || text instanceof String;
+};
+
+const parseDate = (date: unknown) => {
+	if (!date || !isDate(date)) {
+		throw new Error("Incorrect or missing date");
+	}
+
+	return date;
+};
+
+const isDate = (date: string) => {
+	return Boolean(Date.parse(date));
+};
+
+const parseGender = (gender: unknown): Gender => {
+	if (!gender || !isString(gender) || !isGender(gender)) {
+		throw new Error("Incorrect or missing gender: " + gender);
+	}
+
+	return gender;
+};
+
+const isGender = (gender: string): gender is Gender => {
+	return ["male", "female", "others"].includes(gender);
+};

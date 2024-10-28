929d3ee24914781e49e883cec73a7158d264ae37
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-28 18:43:57 +0500

    Validating with zod in toNewPatientData

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { Gender, NewPatientInfo } from "./types";
 
 export const toNewPatientData = (object: unknown): NewPatientInfo => {
@@ -13,69 +14,15 @@ export const toNewPatientData = (object: unknown): NewPatientInfo => {
 		"occupation" in object
 	) {
 		const newPatient: NewPatientInfo = {
-			name: parseName(object.name),
-			dateOfBirth: parseDate(object.dateOfBirth),
-			ssn: parseSSN(object.ssn),
-			gender: parseGender(object.gender),
-			occupation: parseOccupation(object.occupation),
+			name: z.string().parse(object.name),
+			dateOfBirth: z.string().date().parse(object.dateOfBirth),
+			ssn: z.string().parse(object.ssn),
+			gender: z.nativeEnum(Gender).parse(object.gender),
+			occupation: z.string().parse(object.occupation),
 		};
 
 		return newPatient;
 	}
 
 	throw new Error("Incorrect data: some fields are missing");
-};
-
-const parseName = (name: unknown) => {
-	if (!name || !isString(name)) {
-		throw new Error("Incorrect or missing name");
-	}
-
-	return name;
-};
-
-const parseOccupation = (occupation: unknown) => {
-	if (!occupation || !isString(occupation)) {
-		throw new Error("Incorrect or missing occupation");
-	}
-
-	return occupation;
-};
-
-const parseSSN = (ssn: unknown) => {
-	if (!ssn || !isString(ssn)) {
-		throw new Error("Incorrect or missing ssn");
-	}
-
-	return ssn;
-};
-
-const isString = (text: unknown): text is string => {
-	return typeof text === "string" || text instanceof String;
-};
-
-const parseDate = (date: unknown) => {
-	if (!date || !isString(date) || !isDate(date)) {
-		throw new Error("Incorrect or missing date");
-	}
-
-	return date;
-};
-
-const isDate = (date: string): date is string => {
-	return Boolean(Date.parse(date));
-};
-
-const parseGender = (gender: unknown): Gender => {
-	if (!gender || !isString(gender) || !isGender(gender)) {
-		throw new Error("Incorrect or missing gender: " + gender);
-	}
-
-	return gender;
-};
-
-const isGender = (gender: string): gender is Gender => {
-	return Object.values(Gender)
-		.map((g) => g.toString())
-		.includes(gender);
 };

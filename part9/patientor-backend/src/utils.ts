6cda4adc456bf940cb23ba91072acfdff988d19f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-28 18:45:44 +0500

    Use Zod Object schema for validation

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,28 +1,14 @@
 import { z } from "zod";
 import { Gender, NewPatientInfo } from "./types";
 
+const newPatientSchema = z.object({
+	name: z.string(),
+	dateOfBirth: z.string().date(),
+	ssn: z.string(),
+	gender: z.nativeEnum(Gender),
+	occupation: z.string(),
+});
+
 export const toNewPatientData = (object: unknown): NewPatientInfo => {
-	if (!object || typeof object !== "object") {
-		throw new Error("Incorrect or missing data");
-	}
-
-	if (
-		"name" in object &&
-		"dateOfBirth" in object &&
-		"gender" in object &&
-		"ssn" in object &&
-		"occupation" in object
-	) {
-		const newPatient: NewPatientInfo = {
-			name: z.string().parse(object.name),
-			dateOfBirth: z.string().date().parse(object.dateOfBirth),
-			ssn: z.string().parse(object.ssn),
-			gender: z.nativeEnum(Gender).parse(object.gender),
-			occupation: z.string().parse(object.occupation),
-		};
-
-		return newPatient;
-	}
-
-	throw new Error("Incorrect data: some fields are missing");
+	return newPatientSchema.parse(object);
 };

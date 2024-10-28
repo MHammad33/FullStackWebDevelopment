5b9d17a630c782cb18228a3407d34d1e49af18f6
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-28 18:48:31 +0500

    Remove toNewPatientData function in utils

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,14 +1,10 @@
 import { z } from "zod";
-import { Gender, NewPatientInfo } from "./types";
+import { Gender } from "./types";
 
-const newPatientSchema = z.object({
+export const NewPatientSchema = z.object({
 	name: z.string(),
 	dateOfBirth: z.string().date(),
 	ssn: z.string(),
 	gender: z.nativeEnum(Gender),
 	occupation: z.string(),
 });
-
-export const toNewPatientData = (object: unknown): NewPatientInfo => {
-	return newPatientSchema.parse(object);
-};

3695e264630727f33331dc8014d71aed1394e8e3
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-30 18:48:03 +0500

    Add Part component in course

---
+++ part9/course/src/components/Part.tsx
@@ -0,0 +1,46 @@
+import { FC } from "react";
+import { CoursePart } from "../types";
+
+interface PartProps {
+	part: CoursePart;
+}
+
+const Part: FC<PartProps> = ({ part }) => {
+	switch (part.kind) {
+		case "basic":
+			return (
+				<div>
+					<p>
+						<strong>{part.name}</strong> ({part.exerciseCount} exercises)
+					</p>
+					<p>{part.description}</p>
+				</div>
+			);
+		case "group":
+			return (
+				<div>
+					<p>
+						<strong>{part.name}</strong> ({part.exerciseCount} exercises,{" "}
+						{part.groupProjectCount} group projects)
+					</p>
+				</div>
+			);
+		case "background":
+			return (
+				<div>
+					<p>
+						<strong>{part.name}</strong> ({part.exerciseCount} exercises)
+					</p>
+					<p>{part.description}</p>
+					<p>
+						Background material:{" "}
+						<a href={part.backgroundMaterial}>{part.backgroundMaterial}</a>
+					</p>
+				</div>
+			);
+		default:
+			return null;
+	}
+};
+
+export default Part;

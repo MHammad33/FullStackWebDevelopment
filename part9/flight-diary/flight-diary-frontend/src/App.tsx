ab732da2e8aae5d5bc01feb558374c4bd0ed37ad
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-11-02 14:26:19 +0500

    Add GET all diaries logic

--- part9/flight-diary/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import { DiaryEntry } from "./types";
+import { getAllDiaries } from "./service/diaryService";
+
 function App() {
-	return <></>;
+	const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+
+	useEffect(() => {
+		getAllDiaries().then((data) => {
+			setDiaries(data);
+		});
+	}, []);
+
+	return (
+		<>
+			<h1>Diary Entries</h1>
+			{diaries.map((diary) => (
+				<div key={diary.id}>
+					<h2>{diary.date}</h2>
+					<p>Visibility: {diary.visibility}</p>
+					<p>Weather: {diary.weather}</p>
+				</div>
+			))}
+		</>
+	);
 }
 
 export default App;
--- part9/flight-diary/flight-diary-frontend/src/service/diaryService.ts
+++ part9/flight-diary/flight-diary-frontend/src/service/diaryService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { DiaryEntry } from "../types";
 
-const baseUrl = "http://localhost:3001/diaries";
+const baseUrl = "http://localhost:3000/api/diaries";
 
 export const getAllDiaries = async () => {
 	return axios.get<DiaryEntry[]>(baseUrl).then((response) => response.data);

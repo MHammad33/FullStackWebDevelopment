aa94bd8611996b949ddc1c6269b981fd33bb1987
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-11-05 19:07:41 +0500

    Modify types

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,7 +1,27 @@
-export interface DiagnosisInfo {
+export interface Diagnosis {
 	code: string;
 	name: string;
 	latin?: string;
+}
+
+interface BaseEntry {
+	id: string;
+	description: string;
+	date: string;
+	specialist: string;
+	diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+	"Healthy" = 0,
+	"LowRisk" = 1,
+	"HighRisk" = 2,
+	"CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+	type: "HealthCheck";
+	healthCheckRating: HealthCheckRating;
 }
 
 export enum Gender {
@@ -9,9 +29,6 @@ export enum Gender {
 	Female = "female",
 	Other = "others",
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
 
 export interface PatientInfo {
 	id: string;
@@ -23,6 +40,17 @@ export interface PatientInfo {
 	entries: Entry[];
 }
 
+export type Entry =
+	| HospitalEntry
+	| OccupationalHealthcareEntry
+	| HealthCheckEntry;
+
 export type NonSensitivePatient = Omit<PatientInfo, "ssn" | "entries">;
 
 export type NewPatientInfo = Omit<PatientInfo, "id">;
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+	? Omit<T, K>
+	: never;
+
+export type EntryWithoutId = UnionOmit<Entry, "id">;
